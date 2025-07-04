f3deb3bc665f412c20359600f59b8e5a108a8d1b
jgreen2023 <134550521+jgreen2023@users.noreply.github.com>
2025-07-04 13:04:07 -0400

    commit for 7/4/25

--- function.js
+++ function.js
@@ -2,4 +2,21 @@
 // let songlength = 0;
 // let songindex = 0;
 // let songplaying = false;
-// let track = document.createElement("audio");
+// let track = document.createElement("audio");\
+
+
+let angle = 0;
+
+function rotateImage() {
+  const img = document.getElementById('albumArt');
+  setInterval(() => {
+    angle = (angle + 1) % 360;
+    img.style.transform = `rotate(${angle}deg)`;
+  }, 20); 
+}
+
+
+window.onload = rotateImage;
+
+const audio = document.getElementById('myAudio');
+const progressBar = document.getElementById('progressBar');
